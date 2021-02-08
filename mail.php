1fcca1427220f72256368e3e246411b6c62fa201
wixxxez <61543394+wixxxez@users.noreply.github.com>
2021-02-08 23:20:01 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -21,7 +21,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'axilium.team@gmail.com';                     // SMTP username
-    $mail->Password   = '9H0fa2Lbb90iKmaY';                               // SMTP password
+    $mail->Password   = '*****';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
